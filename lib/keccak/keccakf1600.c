d7008ca4657de3ddd41a0b944e27d58c82481b1b
Paweł Bylica <chfast@gmail.com>
2020-12-05 16:43:29 +0100

    keccak: Use the state directly with alias "A"
    This follows naming in FIPS 202: SHA3 standard and OpenSSL.

--- lib/keccak/keccakf1600.c
+++ lib/keccak/keccakf1600.c
@@ -45,11 +45,7 @@ static const uint64_t round_constants[24] = {
 ///   https://keccak.team/archives.html, CC0-1.0 / Public Domain.
 void ethash_keccakf1600(uint64_t state[5][5])
 {
-    uint64_t Aba, Abe, Abi, Abo, Abu;
-    uint64_t Aga, Age, Agi, Ago, Agu;
-    uint64_t Aka, Ake, Aki, Ako, Aku;
-    uint64_t Ama, Ame, Ami, Amo, Amu;
-    uint64_t Asa, Ase, Asi, Aso, Asu;
+    uint64_t(*A)[5] = state;
 
     uint64_t Eba, Ebe, Ebi, Ebo, Ebu;
     uint64_t Ega, Ege, Egi, Ego, Egu;
@@ -61,41 +57,15 @@ void ethash_keccakf1600(uint64_t state[5][5])
 
     uint64_t Da, De, Di, Do, Du;
 
-    Aba = state[0][0];
-    Abe = state[0][1];
-    Abi = state[0][2];
-    Abo = state[0][3];
-    Abu = state[0][4];
-    Aga = state[1][0];
-    Age = state[1][1];
-    Agi = state[1][2];
-    Ago = state[1][3];
-    Agu = state[1][4];
-    Aka = state[2][0];
-    Ake = state[2][1];
-    Aki = state[2][2];
-    Ako = state[2][3];
-    Aku = state[2][4];
-    Ama = state[3][0];
-    Ame = state[3][1];
-    Ami = state[3][2];
-    Amo = state[3][3];
-    Amu = state[3][4];
-    Asa = state[4][0];
-    Ase = state[4][1];
-    Asi = state[4][2];
-    Aso = state[4][3];
-    Asu = state[4][4];
-
     for (int round = 0; round < 24; round += 2)
     {
-        /* Round (round + 0): Axx -> Exx */
+        /* Round (round + 0): A -> Exx */
 
-        Ba = Aba ^ Aga ^ Aka ^ Ama ^ Asa;
-        Be = Abe ^ Age ^ Ake ^ Ame ^ Ase;
-        Bi = Abi ^ Agi ^ Aki ^ Ami ^ Asi;
-        Bo = Abo ^ Ago ^ Ako ^ Amo ^ Aso;
-        Bu = Abu ^ Agu ^ Aku ^ Amu ^ Asu;
+        Ba = A[0][0] ^ A[1][0] ^ A[2][0] ^ A[3][0] ^ A[4][0];
+        Be = A[0][1] ^ A[1][1] ^ A[2][1] ^ A[3][1] ^ A[4][1];
+        Bi = A[0][2] ^ A[1][2] ^ A[2][2] ^ A[3][2] ^ A[4][2];
+        Bo = A[0][3] ^ A[1][3] ^ A[2][3] ^ A[3][3] ^ A[4][3];
+        Bu = A[0][4] ^ A[1][4] ^ A[2][4] ^ A[3][4] ^ A[4][4];
 
         Da = Bu ^ rol(Be, 1);
         De = Ba ^ rol(Bi, 1);
@@ -103,55 +73,55 @@ void ethash_keccakf1600(uint64_t state[5][5])
         Do = Bi ^ rol(Bu, 1);
         Du = Bo ^ rol(Ba, 1);
 
-        Ba = Aba ^ Da;
-        Be = rol(Age ^ De, 44);
-        Bi = rol(Aki ^ Di, 43);
-        Bo = rol(Amo ^ Do, 21);
-        Bu = rol(Asu ^ Du, 14);
+        Ba = A[0][0] ^ Da;
+        Be = rol(A[1][1] ^ De, 44);
+        Bi = rol(A[2][2] ^ Di, 43);
+        Bo = rol(A[3][3] ^ Do, 21);
+        Bu = rol(A[4][4] ^ Du, 14);
         Eba = Ba ^ (~Be & Bi) ^ round_constants[round];
         Ebe = Be ^ (~Bi & Bo);
         Ebi = Bi ^ (~Bo & Bu);
         Ebo = Bo ^ (~Bu & Ba);
         Ebu = Bu ^ (~Ba & Be);
 
-        Ba = rol(Abo ^ Do, 28);
-        Be = rol(Agu ^ Du, 20);
-        Bi = rol(Aka ^ Da, 3);
-        Bo = rol(Ame ^ De, 45);
-        Bu = rol(Asi ^ Di, 61);
+        Ba = rol(A[0][3] ^ Do, 28);
+        Be = rol(A[1][4] ^ Du, 20);
+        Bi = rol(A[2][0] ^ Da, 3);
+        Bo = rol(A[3][1] ^ De, 45);
+        Bu = rol(A[4][2] ^ Di, 61);
         Ega = Ba ^ (~Be & Bi);
         Ege = Be ^ (~Bi & Bo);
         Egi = Bi ^ (~Bo & Bu);
         Ego = Bo ^ (~Bu & Ba);
         Egu = Bu ^ (~Ba & Be);
 
-        Ba = rol(Abe ^ De, 1);
-        Be = rol(Agi ^ Di, 6);
-        Bi = rol(Ako ^ Do, 25);
-        Bo = rol(Amu ^ Du, 8);
-        Bu = rol(Asa ^ Da, 18);
+        Ba = rol(A[0][1] ^ De, 1);
+        Be = rol(A[1][2] ^ Di, 6);
+        Bi = rol(A[2][3] ^ Do, 25);
+        Bo = rol(A[3][4] ^ Du, 8);
+        Bu = rol(A[4][0] ^ Da, 18);
         Eka = Ba ^ (~Be & Bi);
         Eke = Be ^ (~Bi & Bo);
         Eki = Bi ^ (~Bo & Bu);
         Eko = Bo ^ (~Bu & Ba);
         Eku = Bu ^ (~Ba & Be);
 
-        Ba = rol(Abu ^ Du, 27);
-        Be = rol(Aga ^ Da, 36);
-        Bi = rol(Ake ^ De, 10);
-        Bo = rol(Ami ^ Di, 15);
-        Bu = rol(Aso ^ Do, 56);
+        Ba = rol(A[0][4] ^ Du, 27);
+        Be = rol(A[1][0] ^ Da, 36);
+        Bi = rol(A[2][1] ^ De, 10);
+        Bo = rol(A[3][2] ^ Di, 15);
+        Bu = rol(A[4][3] ^ Do, 56);
         Ema = Ba ^ (~Be & Bi);
         Eme = Be ^ (~Bi & Bo);
         Emi = Bi ^ (~Bo & Bu);
         Emo = Bo ^ (~Bu & Ba);
         Emu = Bu ^ (~Ba & Be);
 
-        Ba = rol(Abi ^ Di, 62);
-        Be = rol(Ago ^ Do, 55);
-        Bi = rol(Aku ^ Du, 39);
-        Bo = rol(Ama ^ Da, 41);
-        Bu = rol(Ase ^ De, 2);
+        Ba = rol(A[0][2] ^ Di, 62);
+        Be = rol(A[1][3] ^ Do, 55);
+        Bi = rol(A[2][4] ^ Du, 39);
+        Bo = rol(A[3][0] ^ Da, 41);
+        Bu = rol(A[4][1] ^ De, 2);
         Esa = Ba ^ (~Be & Bi);
         Ese = Be ^ (~Bi & Bo);
         Esi = Bi ^ (~Bo & Bu);
@@ -159,7 +129,7 @@ void ethash_keccakf1600(uint64_t state[5][5])
         Esu = Bu ^ (~Ba & Be);
 
 
-        /* Round (round + 1): Exx -> Axx */
+        /* Round (round + 1): Exx -> A */
 
         Ba = Eba ^ Ega ^ Eka ^ Ema ^ Esa;
         Be = Ebe ^ Ege ^ Eke ^ Eme ^ Ese;
@@ -178,80 +148,54 @@ void ethash_keccakf1600(uint64_t state[5][5])
         Bi = rol(Eki ^ Di, 43);
         Bo = rol(Emo ^ Do, 21);
         Bu = rol(Esu ^ Du, 14);
-        Aba = Ba ^ (~Be & Bi) ^ round_constants[round + 1];
-        Abe = Be ^ (~Bi & Bo);
-        Abi = Bi ^ (~Bo & Bu);
-        Abo = Bo ^ (~Bu & Ba);
-        Abu = Bu ^ (~Ba & Be);
+        A[0][0] = Ba ^ (~Be & Bi) ^ round_constants[round + 1];
+        A[0][1] = Be ^ (~Bi & Bo);
+        A[0][2] = Bi ^ (~Bo & Bu);
+        A[0][3] = Bo ^ (~Bu & Ba);
+        A[0][4] = Bu ^ (~Ba & Be);
 
         Ba = rol(Ebo ^ Do, 28);
         Be = rol(Egu ^ Du, 20);
         Bi = rol(Eka ^ Da, 3);
         Bo = rol(Eme ^ De, 45);
         Bu = rol(Esi ^ Di, 61);
-        Aga = Ba ^ (~Be & Bi);
-        Age = Be ^ (~Bi & Bo);
-        Agi = Bi ^ (~Bo & Bu);
-        Ago = Bo ^ (~Bu & Ba);
-        Agu = Bu ^ (~Ba & Be);
+        A[1][0] = Ba ^ (~Be & Bi);
+        A[1][1] = Be ^ (~Bi & Bo);
+        A[1][2] = Bi ^ (~Bo & Bu);
+        A[1][3] = Bo ^ (~Bu & Ba);
+        A[1][4] = Bu ^ (~Ba & Be);
 
         Ba = rol(Ebe ^ De, 1);
         Be = rol(Egi ^ Di, 6);
         Bi = rol(Eko ^ Do, 25);
         Bo = rol(Emu ^ Du, 8);
         Bu = rol(Esa ^ Da, 18);
-        Aka = Ba ^ (~Be & Bi);
-        Ake = Be ^ (~Bi & Bo);
-        Aki = Bi ^ (~Bo & Bu);
-        Ako = Bo ^ (~Bu & Ba);
-        Aku = Bu ^ (~Ba & Be);
+        A[2][0] = Ba ^ (~Be & Bi);
+        A[2][1] = Be ^ (~Bi & Bo);
+        A[2][2] = Bi ^ (~Bo & Bu);
+        A[2][3] = Bo ^ (~Bu & Ba);
+        A[2][4] = Bu ^ (~Ba & Be);
 
         Ba = rol(Ebu ^ Du, 27);
         Be = rol(Ega ^ Da, 36);
         Bi = rol(Eke ^ De, 10);
         Bo = rol(Emi ^ Di, 15);
         Bu = rol(Eso ^ Do, 56);
-        Ama = Ba ^ (~Be & Bi);
-        Ame = Be ^ (~Bi & Bo);
-        Ami = Bi ^ (~Bo & Bu);
-        Amo = Bo ^ (~Bu & Ba);
-        Amu = Bu ^ (~Ba & Be);
+        A[3][0] = Ba ^ (~Be & Bi);
+        A[3][1] = Be ^ (~Bi & Bo);
+        A[3][2] = Bi ^ (~Bo & Bu);
+        A[3][3] = Bo ^ (~Bu & Ba);
+        A[3][4] = Bu ^ (~Ba & Be);
 
         Ba = rol(Ebi ^ Di, 62);
         Be = rol(Ego ^ Do, 55);
         Bi = rol(Eku ^ Du, 39);
         Bo = rol(Ema ^ Da, 41);
         Bu = rol(Ese ^ De, 2);
-        Asa = Ba ^ (~Be & Bi);
-        Ase = Be ^ (~Bi & Bo);
-        Asi = Bi ^ (~Bo & Bu);
-        Aso = Bo ^ (~Bu & Ba);
-        Asu = Bu ^ (~Ba & Be);
+        A[4][0] = Ba ^ (~Be & Bi);
+        A[4][1] = Be ^ (~Bi & Bo);
+        A[4][2] = Bi ^ (~Bo & Bu);
+        A[4][3] = Bo ^ (~Bu & Ba);
+        A[4][4] = Bu ^ (~Ba & Be);
     }
-
-    state[0][0] = Aba;
-    state[0][1] = Abe;
-    state[0][2] = Abi;
-    state[0][3] = Abo;
-    state[0][4] = Abu;
-    state[1][0] = Aga;
-    state[1][1] = Age;
-    state[1][2] = Agi;
-    state[1][3] = Ago;
-    state[1][4] = Agu;
-    state[2][0] = Aka;
-    state[2][1] = Ake;
-    state[2][2] = Aki;
-    state[2][3] = Ako;
-    state[2][4] = Aku;
-    state[3][0] = Ama;
-    state[3][1] = Ame;
-    state[3][2] = Ami;
-    state[3][3] = Amo;
-    state[3][4] = Amu;
-    state[4][0] = Asa;
-    state[4][1] = Ase;
-    state[4][2] = Asi;
-    state[4][3] = Aso;
-    state[4][4] = Asu;
 }
