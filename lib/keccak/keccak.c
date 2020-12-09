3fa52fc75b1fb2df1b156c33d1ccf27dc42a5e31
Paweł Bylica <chfast@gmail.com>
2020-12-09 10:08:20 +0100

    A

--- lib/keccak/keccak.c
+++ lib/keccak/keccak.c
@@ -55,14 +55,8 @@ static const uint64_t round_constants[24] = {  //
 /// The implementation based on:
 /// - "simple" implementation by Ronny Van Keer, included in "Reference and optimized code in C",
 ///   https://keccak.team/archives.html, CC0-1.0 / Public Domain.
-static inline ALWAYS_INLINE void keccakf1600_implementation(uint64_t state[25])
+static inline ALWAYS_INLINE void keccakf1600_implementation(uint64_t A[25])
 {
-    uint64_t Aba, Abe, Abi, Abo, Abu;
-    uint64_t Aga, Age, Agi, Ago, Agu;
-    uint64_t Aka, Ake, Aki, Ako, Aku;
-    uint64_t Ama, Ame, Ami, Amo, Amu;
-    uint64_t Asa, Ase, Asi, Aso, Asu;
-
     uint64_t Eba, Ebe, Ebi, Ebo, Ebu;
     uint64_t Ega, Ege, Egi, Ego, Egu;
     uint64_t Eka, Eke, Eki, Eko, Eku;
@@ -73,41 +67,15 @@ static inline ALWAYS_INLINE void keccakf1600_implementation(uint64_t state[25])
 
     uint64_t Da, De, Di, Do, Du;
 
-    Aba = state[0];
-    Abe = state[1];
-    Abi = state[2];
-    Abo = state[3];
-    Abu = state[4];
-    Aga = state[5];
-    Age = state[6];
-    Agi = state[7];
-    Ago = state[8];
-    Agu = state[9];
-    Aka = state[10];
-    Ake = state[11];
-    Aki = state[12];
-    Ako = state[13];
-    Aku = state[14];
-    Ama = state[15];
-    Ame = state[16];
-    Ami = state[17];
-    Amo = state[18];
-    Amu = state[19];
-    Asa = state[20];
-    Ase = state[21];
-    Asi = state[22];
-    Aso = state[23];
-    Asu = state[24];
-
     for (size_t n = 0; n < 24; n += 2)
     {
         // Round (n + 0): Axx -> Exx
 
-        Ba = Aba ^ Aga ^ Aka ^ Ama ^ Asa;
-        Be = Abe ^ Age ^ Ake ^ Ame ^ Ase;
-        Bi = Abi ^ Agi ^ Aki ^ Ami ^ Asi;
-        Bo = Abo ^ Ago ^ Ako ^ Amo ^ Aso;
-        Bu = Abu ^ Agu ^ Aku ^ Amu ^ Asu;
+        Ba = A[0] ^ A[5] ^ A[10] ^ A[15] ^ A[20];
+        Be = A[1] ^ A[6] ^ A[11] ^ A[16] ^ A[21];
+        Bi = A[2] ^ A[7] ^ A[12] ^ A[17] ^ A[22];
+        Bo = A[3] ^ A[8] ^ A[13] ^ A[18] ^ A[23];
+        Bu = A[4] ^ A[9] ^ A[14] ^ A[19] ^ A[24];
 
         Da = Bu ^ rol(Be, 1);
         De = Ba ^ rol(Bi, 1);
@@ -115,55 +83,55 @@ static inline ALWAYS_INLINE void keccakf1600_implementation(uint64_t state[25])
         Do = Bi ^ rol(Bu, 1);
         Du = Bo ^ rol(Ba, 1);
 
-        Ba = Aba ^ Da;
-        Be = rol(Age ^ De, 44);
-        Bi = rol(Aki ^ Di, 43);
-        Bo = rol(Amo ^ Do, 21);
-        Bu = rol(Asu ^ Du, 14);
+        Ba = A[0] ^ Da;
+        Be = rol(A[6] ^ De, 44);
+        Bi = rol(A[12] ^ Di, 43);
+        Bo = rol(A[18] ^ Do, 21);
+        Bu = rol(A[24] ^ Du, 14);
         Eba = Ba ^ (~Be & Bi) ^ round_constants[n];
         Ebe = Be ^ (~Bi & Bo);
         Ebi = Bi ^ (~Bo & Bu);
         Ebo = Bo ^ (~Bu & Ba);
         Ebu = Bu ^ (~Ba & Be);
 
-        Ba = rol(Abo ^ Do, 28);
-        Be = rol(Agu ^ Du, 20);
-        Bi = rol(Aka ^ Da, 3);
-        Bo = rol(Ame ^ De, 45);
-        Bu = rol(Asi ^ Di, 61);
+        Ba = rol(A[3] ^ Do, 28);
+        Be = rol(A[9] ^ Du, 20);
+        Bi = rol(A[10] ^ Da, 3);
+        Bo = rol(A[16] ^ De, 45);
+        Bu = rol(A[22] ^ Di, 61);
         Ega = Ba ^ (~Be & Bi);
         Ege = Be ^ (~Bi & Bo);
         Egi = Bi ^ (~Bo & Bu);
         Ego = Bo ^ (~Bu & Ba);
         Egu = Bu ^ (~Ba & Be);
 
-        Ba = rol(Abe ^ De, 1);
-        Be = rol(Agi ^ Di, 6);
-        Bi = rol(Ako ^ Do, 25);
-        Bo = rol(Amu ^ Du, 8);
-        Bu = rol(Asa ^ Da, 18);
+        Ba = rol(A[1] ^ De, 1);
+        Be = rol(A[7] ^ Di, 6);
+        Bi = rol(A[13] ^ Do, 25);
+        Bo = rol(A[19] ^ Du, 8);
+        Bu = rol(A[20] ^ Da, 18);
         Eka = Ba ^ (~Be & Bi);
         Eke = Be ^ (~Bi & Bo);
         Eki = Bi ^ (~Bo & Bu);
         Eko = Bo ^ (~Bu & Ba);
         Eku = Bu ^ (~Ba & Be);
 
-        Ba = rol(Abu ^ Du, 27);
-        Be = rol(Aga ^ Da, 36);
-        Bi = rol(Ake ^ De, 10);
-        Bo = rol(Ami ^ Di, 15);
-        Bu = rol(Aso ^ Do, 56);
+        Ba = rol(A[4] ^ Du, 27);
+        Be = rol(A[5] ^ Da, 36);
+        Bi = rol(A[11] ^ De, 10);
+        Bo = rol(A[17] ^ Di, 15);
+        Bu = rol(A[23] ^ Do, 56);
         Ema = Ba ^ (~Be & Bi);
         Eme = Be ^ (~Bi & Bo);
         Emi = Bi ^ (~Bo & Bu);
         Emo = Bo ^ (~Bu & Ba);
         Emu = Bu ^ (~Ba & Be);
 
-        Ba = rol(Abi ^ Di, 62);
-        Be = rol(Ago ^ Do, 55);
-        Bi = rol(Aku ^ Du, 39);
-        Bo = rol(Ama ^ Da, 41);
-        Bu = rol(Ase ^ De, 2);
+        Ba = rol(A[2] ^ Di, 62);
+        Be = rol(A[8] ^ Do, 55);
+        Bi = rol(A[14] ^ Du, 39);
+        Bo = rol(A[15] ^ Da, 41);
+        Bu = rol(A[21] ^ De, 2);
         Esa = Ba ^ (~Be & Bi);
         Ese = Be ^ (~Bi & Bo);
         Esi = Bi ^ (~Bo & Bu);
@@ -190,82 +158,56 @@ static inline ALWAYS_INLINE void keccakf1600_implementation(uint64_t state[25])
         Bi = rol(Eki ^ Di, 43);
         Bo = rol(Emo ^ Do, 21);
         Bu = rol(Esu ^ Du, 14);
-        Aba = Ba ^ (~Be & Bi) ^ round_constants[n + 1];
-        Abe = Be ^ (~Bi & Bo);
-        Abi = Bi ^ (~Bo & Bu);
-        Abo = Bo ^ (~Bu & Ba);
-        Abu = Bu ^ (~Ba & Be);
+        A[0] = Ba ^ (~Be & Bi) ^ round_constants[n + 1];
+        A[1] = Be ^ (~Bi & Bo);
+        A[2] = Bi ^ (~Bo & Bu);
+        A[3] = Bo ^ (~Bu & Ba);
+        A[4] = Bu ^ (~Ba & Be);
 
         Ba = rol(Ebo ^ Do, 28);
         Be = rol(Egu ^ Du, 20);
         Bi = rol(Eka ^ Da, 3);
         Bo = rol(Eme ^ De, 45);
         Bu = rol(Esi ^ Di, 61);
-        Aga = Ba ^ (~Be & Bi);
-        Age = Be ^ (~Bi & Bo);
-        Agi = Bi ^ (~Bo & Bu);
-        Ago = Bo ^ (~Bu & Ba);
-        Agu = Bu ^ (~Ba & Be);
+        A[5] = Ba ^ (~Be & Bi);
+        A[6] = Be ^ (~Bi & Bo);
+        A[7] = Bi ^ (~Bo & Bu);
+        A[8] = Bo ^ (~Bu & Ba);
+        A[9] = Bu ^ (~Ba & Be);
 
         Ba = rol(Ebe ^ De, 1);
         Be = rol(Egi ^ Di, 6);
         Bi = rol(Eko ^ Do, 25);
         Bo = rol(Emu ^ Du, 8);
         Bu = rol(Esa ^ Da, 18);
-        Aka = Ba ^ (~Be & Bi);
-        Ake = Be ^ (~Bi & Bo);
-        Aki = Bi ^ (~Bo & Bu);
-        Ako = Bo ^ (~Bu & Ba);
-        Aku = Bu ^ (~Ba & Be);
+        A[10] = Ba ^ (~Be & Bi);
+        A[11] = Be ^ (~Bi & Bo);
+        A[12] = Bi ^ (~Bo & Bu);
+        A[13] = Bo ^ (~Bu & Ba);
+        A[14] = Bu ^ (~Ba & Be);
 
         Ba = rol(Ebu ^ Du, 27);
         Be = rol(Ega ^ Da, 36);
         Bi = rol(Eke ^ De, 10);
         Bo = rol(Emi ^ Di, 15);
         Bu = rol(Eso ^ Do, 56);
-        Ama = Ba ^ (~Be & Bi);
-        Ame = Be ^ (~Bi & Bo);
-        Ami = Bi ^ (~Bo & Bu);
-        Amo = Bo ^ (~Bu & Ba);
-        Amu = Bu ^ (~Ba & Be);
+        A[15] = Ba ^ (~Be & Bi);
+        A[16] = Be ^ (~Bi & Bo);
+        A[17] = Bi ^ (~Bo & Bu);
+        A[18] = Bo ^ (~Bu & Ba);
+        A[19] = Bu ^ (~Ba & Be);
 
         Ba = rol(Ebi ^ Di, 62);
         Be = rol(Ego ^ Do, 55);
         Bi = rol(Eku ^ Du, 39);
         Bo = rol(Ema ^ Da, 41);
         Bu = rol(Ese ^ De, 2);
-        Asa = Ba ^ (~Be & Bi);
-        Ase = Be ^ (~Bi & Bo);
-        Asi = Bi ^ (~Bo & Bu);
-        Aso = Bo ^ (~Bu & Ba);
-        Asu = Bu ^ (~Ba & Be);
+        A[20] = Ba ^ (~Be & Bi);
+        A[21] = Be ^ (~Bi & Bo);
+        A[22] = Bi ^ (~Bo & Bu);
+        A[23] = Bo ^ (~Bu & Ba);
+        A[24] = Bu ^ (~Ba & Be);
     }
-
-    state[0] = Aba;
-    state[1] = Abe;
-    state[2] = Abi;
-    state[3] = Abo;
-    state[4] = Abu;
-    state[5] = Aga;
-    state[6] = Age;
-    state[7] = Agi;
-    state[8] = Ago;
-    state[9] = Agu;
-    state[10] = Aka;
-    state[11] = Ake;
-    state[12] = Aki;
-    state[13] = Ako;
-    state[14] = Aku;
-    state[15] = Ama;
-    state[16] = Ame;
-    state[17] = Ami;
-    state[18] = Amo;
-    state[19] = Amu;
-    state[20] = Asa;
-    state[21] = Ase;
-    state[22] = Asi;
-    state[23] = Aso;
-    state[24] = Asu;
 }
 
 static void keccakf1600_generic(uint64_t state[25])
